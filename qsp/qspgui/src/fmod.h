caa3606aa11cb5c0cd0cfe20bf531ae8651d4005
Byte <hzdbyte@gmail.com>
2008-12-20 16:04:08 +0000

    FMOD updated to 4.20.06

--- qsp/qspgui/src/fmod.h
+++ qsp/qspgui/src/fmod.h
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00042003
+#define FMOD_VERSION    0x00042006
 
 /*
     Compiler specific settings.
