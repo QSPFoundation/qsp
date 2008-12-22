094c84b661deb5f1afa146c81f4124b15e0ac9de
Byte <hzdbyte@gmail.com>
2008-12-22 15:54:48 +0000

    Improved wx's build script for win32

--- qsp/build_wx/win32/update.cmd
+++ qsp/build_wx/win32/update.cmd
@@ -1,13 +1,15 @@
+SET WX_PATH=d:\wx\wxWidgets
+SET WX_QSP_PATH=d:\wx\wx_qsp
 SET MSYS=c:\msys
 SET VC=c:\Program Files\Microsoft Visual Studio 9.0
+
+SET QSP_PATH=%~dp0\..\..
 rem --------------------------------------------------------------------------
-d:
-cd d:\wx
-svn up wxWidgets
-rd /s /q wx_qsp
-svn export wxWidgets wx_qsp
-"%MSYS%\bin\patch" -i d:\wx\wxPatch.diff -d d:\wx\wx_qsp -p 3 --binary
-cd wx_qsp\build\msw
+svn up "%WX_PATH%"
+rd /s /q "%WX_QSP_PATH%"
+svn export "%WX_PATH%" "%WX_QSP_PATH%"
+"%MSYS%\bin\patch" -i "%QSP_PATH%/build_wx/wxPatch.diff" -d "%WX_QSP_PATH%" -p 3 --binary
+cd /d "%WX_QSP_PATH%\build\msw"
 rem MinGW --------------------------------------------------------------------
 SET PATH=%MSYS%\bin;%PATH%
 mingw32-make -f makefile.gcc BUILD=release UNICODE=1 RUNTIME_LIBS=static
