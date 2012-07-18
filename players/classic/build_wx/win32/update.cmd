60acad657bd151affd111419857234938843fa34
Byte <hzdbyte@gmail.com>
2012-07-18 02:19:07 +0000

    Fix update script; Use fixed wxWidgets revision

--- players/classic/build_wx/win32/update.cmd
+++ players/classic/build_wx/win32/update.cmd
@@ -1,17 +1,14 @@
-SET WX_PATH=d:\wx\wxWidgets
-SET WX_QSP_PATH=d:\wx\wx_qsp
 SET MSYS=c:\msys
 SET VC=c:\Program Files\Microsoft Visual Studio 9.0
 
-SET QSP_PATH=%~dp0\..\..
+SET BUILD_PATH=%~dp0\..
+SET WX_PATH=%BUILD_PATH%\wxWidgets
+
+SET PATH=%PATH%;%MSYS%\bin
 rem --------------------------------------------------------------------------
-svn up "%WX_PATH%"
-rd /s /q "%WX_QSP_PATH%"
-svn export "%WX_PATH%" "%WX_QSP_PATH%"
-"%MSYS%\bin\patch" -i "%QSP_PATH%/build_wx/wxPatch.diff" -d "%WX_QSP_PATH%" -p 3 --binary
-cd /d "%WX_QSP_PATH%\build\msw"
+patch -i "%BUILD_PATH%\wxPatch.diff" -d "%WX_PATH%" -p 0
+cd /d "%WX_PATH%\build\msw"
 rem MinGW --------------------------------------------------------------------
-SET PATH=%MSYS%\bin;%PATH%
 mingw32-make -f makefile.gcc BUILD=release UNICODE=1 RUNTIME_LIBS=static
 rem VC -----------------------------------------------------------------------
 call "%VC%\Common7\Tools\vsvars32.bat"
