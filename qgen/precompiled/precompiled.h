049d55899369808292ebc93fdf98a68d2e016594
Byte <hzdbyte@gmail.com>
2024-09-10 00:13:04 +0200

    Move qgen to it's own repository

--- qgen/precompiled/precompiled.h
+++
@@ -1,9 +0,0 @@
-#ifndef __QGEN_PRECOMPILED_H__
-#define __QGEN_PRECOMPILED_H__
-
-#include <QtGui>
-#include <QtGui/QApplication>
-#include <QtGui/QMainWindow>
-#include <QtGui/QMenuBar>
-
-#endif // __QGEN_PRECOMPILED_H__
